834edd82b7260a6defb5f3d439c7cf842c60b0d2
Hem Kant <h.kant@contentbloom.com>
2017-07-27 00:37:41 +0530

    Component and Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+            <h1> How to create template in Angular and render data from array using ngFor </h1>
+            <ul>
+                <li *ngFor="let arr of myArr">{{ arr }}</li>
+            </ul>  
+            
+            <ul>
+                <li *ngIf="myArr">Array contains records =True!!!</li>
+            </ul>
+
+            <div *ngIf="myArr1; then tmpl1 else tmpl2"></div>
+            <ng-template #tmpl1>myArr1 is set to True</ng-template>
+            <ng-template #tmpl2>myArr1 is set to False</ng-template>
+            
+            `,
   styleUrls: ['./app.component.css']
-})
+  })
 export class AppComponent {
-  title = 'app';
+  myArr=['Name :- Hem Kant','Designation :- Technical Constulant','Company Name :- Content Bloom Inc.'];
+  myArr1=true;
 }
